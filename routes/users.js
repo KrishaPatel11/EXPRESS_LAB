7cb309b9397159dd2d6ca013c16cf947d67f21ac
Krisha Patel <patelkrisha1114@gmail.com>
2025-11-17 14:21:28 -0500

    Updated users routes, views folder, new.ejs and list.ejs

---
+++ routes/users.js
@@ -0,0 +1,43 @@
+router.get('/', (req, res)=>{
+    res.render('users/list', {users:users});
+});
+
+router.get('/new', (req, res)=>{
+    res.send('New User Form');
+});
+
+router.post('/', (req, res)=>{
+    const firstName = req.body.firstName;
+    const isValid = firstName !== ""; //Check if they are there, added
+    if(isValid)
+    {
+        console.log(`Adding user: ${firstName}`);
+        users.push({name:firstName});
+        console.log(`New Set of Users: ${users}`);
+        res.send("User Created!");
+    }
+    else{
+        console.log("Error adding user!");
+        res.render("users/new", {firstName:firstName});
+    }
+});
+
+// router.get('/:id', (req, res)=>{
+//     res.send(`Getting User data: ${req.params.id}`);
+// });
+router.route('/:id').get((req, res)=>{
+    res.send(`Getting User data: ${req.params.id}`);
+}).delete((req, res)=>{
+    res.send(`Deleting user with id: ${req.params.id}`);
+}).put((req, res)=>{
+    res.send(`Updating user with id: ${req.params.id}`);
+});
+
+const users =[ {name:"George"},{name:"Justyna"} ];
+
+router.param("id", (req, res, next, id)=>{
+    console.log(`Accessing user #${id}`);
+    next();
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const userRouter = require('./routes/users');
+const postRouter = require('./routes/post');
+
 const app = express();
-
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+app.use(logger);
 
-const postsRouter = require('./routes/posts');
-app.use('/posts', postsRouter);
+app.use('/users', userRouter);
+app.use('/posts',postRouter);
 
-app.get('/', (req, res) => {
-  console.log('Here');
-  res.render('index', { user: "George!" });
+app.get('/', (req, res)=>{
+    console.log('Here');
+    res.render("index",
+        {user:"George",});
 });
 
-app.listen(3030, () => {
-  console.log('Server running on http://localhost:3030');
+app.get('/new', (req, res)=>{
+    res.render('users/new', {firstName:""})
 });
+
+app.listen(3030);
+
+function logger(req, res, next){
+    console.log(`Page Accessed: ${req.originalUrl}`);
+    next();
+}
